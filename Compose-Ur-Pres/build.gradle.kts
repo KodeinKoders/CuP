45a6d163f2eb88b48d11c1cac86269476232920d
Salomon BRYS <salomon.brys@gmail.com>
2024-04-15 20:28:31 +0200

    ptml

--- Compose-Ur-Pres/build.gradle.kts
+++ Compose-Ur-Pres/build.gradle.kts
@@ -8,4 +8,25 @@ plugins {
 allprojects {
     group = "net.kodein.cup"
     version = "1.0.0-Beta-01"
+
+    /*
+     In a composite build, tasks run from the root will not automatically propagate to subprojects (see
+     https://github.com/gradle/gradle/issues/20863).
+     This plugin is for root build scripts that do not themselves have a "publish" task. It adds a
+     "publish" task that depends on the "publish" tasks of all subprojects, to emulate typical Gradle
+     behavior.
+    */
+    afterEvaluate {
+        listOf(
+            "publish" to "publishing",
+            "publishToMavenLocal" to "publishing"
+        ).forEach { (taskName, taskGroup) ->
+            if (taskName !in project.tasks.names) {
+                tasks.register(taskName) {
+                    group = taskGroup
+                    dependsOn(subprojects.map { ":${it.path}:$taskName" })
+                }
+            }
+        }
+    }
 }
